78409adc63f9d5dd319899a62303c2e9cf320b48
John Garrett <garrettj403@gmail.com>
2025-02-12 20:26:51 -0800

    Add emacs init file

---
+++ emacs/init.el
@@ -0,0 +1,81 @@
+;; .emacs.d/init.el
+;; copied from: https://realpython.com/emacs-the-best-python-editor/#installation-and-basics
+
+;; ===================================
+;; MELPA Package Support
+;; ===================================
+;; Enables basic packaging support
+(require 'package)
+
+;; Adds the Melpa archive to the list of available repositories
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+;; Initializes the package infrastructure
+(package-initialize)
+
+;; If there are no archived package contents, refresh them
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Installs packages
+;; myPackages contains a list of package names
+(defvar myPackages
+  '(better-defaults                 ;; Set up some better Emacs defaults
+    elpy                            ;; Emacs Lisp Python Environment
+    ein                             ;; Emacs iPython Notebook
+    flycheck                        ;; On the fly syntax checking
+    py-autopep8                     ;; Run autopep8 on save
+    blacken                         ;; Black formatting on save
+    magit                           ;; Git integration
+    ;;material-theme                  ;; Theme
+    slime
+    zenburn-theme
+    )
+  )
+
+;; Scans the list in myPackages
+;; If the package listed is not already installed, install it
+(mapc #'(lambda (package)
+          (unless (package-installed-p package)
+            (package-install package)))
+      myPackages)
+
+;; ===================================
+;; Basic Customization
+;; ===================================
+
+(setq inhibit-startup-message t)    ;; Hide the startup message
+; (load-theme 'material t)            ;; Load material theme
+(load-theme 'zenburn t)
+;; (global-linum-mode t)               ;; Enable line numbers globally
+(pixel-scroll-precision-mode)
+
+;; ====================================
+;; Development Setup: Lisp / slime
+;; ====================================
+
+(load (expand-file-name "~/.quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(setq debug-on-error t)
+
+;; ====================================
+;; Development Setup: Python
+;; ====================================
+
+;; Enable elpy
+(elpy-enable)
+
+;; Use IPython for REPL
+(setq python-shell-interpreter "jupyter"
+      python-shell-interpreter-args "console --simple-prompt"
+      python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters
+             "jupyter")
+
+;; Enable Flycheck
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; User-Defined init.el ends here
